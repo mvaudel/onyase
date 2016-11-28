b9f82d3af68cffec5a242fbd7428a37e38a06f13
mvaudel <mvaudel@gmail.com>
2016-11-28 23:46:28 +0100

    Extended the script for the figure of the review.

--- R/SearchSpaceIllustration.R
+++ R/SearchSpaceIllustration.R
@@ -1,8 +1,65 @@
 library(ggplot2)
 
-matches <- read.table(file="matches.txt", sep = "\t", header = T)
+matches <- read.table(file="matches.txt", sep = "\t", header = T, stringsAsFactors = F)
+knownScores[knownScores$eValue == "null", "eValue"] <- NA
+knownScores$eValue <- as.numeric(knownScores$eValue)
+
+knownScores <- read.table(file="scoresComparison.txt", sep = "\t", header = T, stringsAsFactors = F)
+knownScores[knownScores$New.score == "null","New.score"] <- NA
+knownScores[knownScores$New.eValue == "null","New.eValue"] <- NA
+knownScores$New.score <- as.numeric(knownScores$New.score)
+knownScores$New.eValue <- as.numeric(knownScores$New.eValue)
+
+nMatchesData <- read.table(file="nMatches.txt", sep = "\t", header = T, stringsAsFactors = F)
+nMatchesPlot <- ggplot()
+nMatchesPlot <- nMatchesPlot + geom_histogram(aes(x=nMatchesData$nMatches), fill = "darkblue", col = "darkblue", alpha = 0.5)
+nMatchesPlot <- nMatchesPlot + labs(x = "Number of peptides per precursor", y = "number of spectra")
+plot(nMatchesPlot)
 
 ms1Plot <- ggplot()
-ms1Plot <- ms1Plot + geom_violin(aes(x=matches$Category, y = matches$MS1_deviation), width = 0.6, alpha = 0.5)
+ms1Plot <- ms1Plot + geom_density(aes(x=matches$MS1_deviation, col=matches$Category, fill = matches$Category), alpha = 0.5)
 ms1Plot <- ms1Plot + labs(x = "", y = "MS1 deviation [ppm]")
 plot(ms1Plot)
+
+fittingData <- read.table(file="C:\\Projects\\scores\\comparison xtandem\\fitting_values.txt", sep = "\t", header = T)
+abPlot <- ggplot()
+abPlot <- abPlot + geom_point(aes(x=fittingData$a, y=fittingData$b), alpha = 0.1, na.rm=TRUE)
+abPlot <- abPlot + labs(x = "a", y = "b")
+plot(abPlot)
+
+
+valuesData <- read.table(file="C:\\Projects\\scores\\comparison xtandem\\debug_values.txt", sep = "\t", header = T)
+cumulativePPlot <- ggplot()
+cumulativePPlot <- cumulativePPlot + geom_point(aes(x=log(valuesData$X), y=valuesData$Y), col="darkred", alpha = 0.5, na.rm=TRUE)
+cumulativePPlot <- cumulativePPlot + labs(x = "Hyperscore", y = "# Peptides")
+# cumulativePPlot <- cumulativePPlot + lims(x = c(-1,1))
+plot(cumulativePPlot)
+
+histogramData <- read.table(file="C:\\Projects\\scores\\comparison xtandem\\debug_histogram.txt", sep = "\t", header = T)
+histogramPlot <- ggplot()
+histogramPlot <- histogramPlot + geom_smooth(aes(x=histogramData$X, y=histogramData$Y), method='lm', na.rm=TRUE)
+histogramPlot <- histogramPlot + geom_point(aes(x=histogramData$X, y=histogramData$Y), col="darkgreen", alpha = 0.5, na.rm=TRUE)
+histogramPlot <- histogramPlot + labs(x = "Hyperscore", y = "# Peptides")
+plot(histogramPlot)
+
+ms2ScorePlot <- ggplot()
+ms2ScorePlot <- ms2ScorePlot + geom_density(aes(x=log(matches$MS2_Score), col=matches$Category, fill = matches$Category), alpha = 0.1, na.rm=TRUE)
+ms2ScorePlot <- ms2ScorePlot + labs(x = "", y = "Hyperscore")
+ms2ScorePlot <- ms2ScorePlot + lims(y = c(0, 0.005))
+plot(ms2ScorePlot)
+
+ms2EValuePlot <- ggplot()
+ms2EValuePlot <- ms2EValuePlot + geom_density(aes(x=log(matches$eValue), col=matches$Category, fill = matches$Category), alpha = 0.1, na.rm=TRUE)
+ms2EValuePlot <- ms2EValuePlot + labs(x = "", y = "e-value")
+ms2EValuePlot <- ms2EValuePlot + lims(y = c(0, 0.005))
+plot(ms2EValuePlot)
+
+knownScores$psLog <- log(knownScores$PS.score)
+knownScores$pDist <- abs(knownScores$psLog - knownScores$New.eValue) 
+correlationPlot <- ggplot()
+correlationPlot <- correlationPlot + geom_point(aes(x=knownScores$psLog[knownScores$psLog>-34], y=knownScores$New.eValue[knownScores$psLog>-34], col = knownScores$pDist[knownScores$psLog>-34]), alpha = 0.5, na.rm=TRUE)
+# correlationPlot <- correlationPlot + geom_point(aes(x=knownScores$psLog, y=knownScores$New.eValue, col = knownScores$pDist), alpha = 0.5, na.rm=TRUE)
+correlationPlot <- correlationPlot + labs(x = "p1", y = "p2", col="Difference")
+# correlationPlot <- correlationPlot + lims(y = c(1e-10,1))
+plot(correlationPlot)
+
--- src/main/java/no/uib/onyase/scripts/ReviewFigure.java
+++ src/main/java/no/uib/onyase/scripts/ReviewFigure.java
@@ -10,8 +10,9 @@ import com.compomics.util.experiment.identification.identification_parameters.Se
 import com.compomics.util.experiment.identification.peptide_fragmentation.PeptideFragmentationModel;
 import com.compomics.util.experiment.identification.protein_sequences.ProteinSequenceIterator;
 import com.compomics.util.experiment.identification.protein_sequences.SequenceFactory;
-import com.compomics.util.experiment.identification.psm_scoring.psm_scores.FastXcorr;
+import com.compomics.util.experiment.identification.psm_scoring.psm_scores.HyperScore;
 import com.compomics.util.experiment.identification.spectrum_annotation.AnnotationSettings;
+import com.compomics.util.experiment.identification.spectrum_annotation.NeutralLossesMap;
 import com.compomics.util.experiment.identification.spectrum_annotation.SpecificAnnotationSettings;
 import com.compomics.util.experiment.identification.spectrum_annotation.spectrum_annotators.PeptideSpectrumAnnotator;
 import com.compomics.util.experiment.identification.spectrum_assumptions.PeptideAssumption;
@@ -24,8 +25,10 @@ import com.compomics.util.experiment.massspectrometry.indexes.PrecursorMap;
 import com.compomics.util.preferences.DigestionPreferences;
 import com.compomics.util.preferences.IdentificationParameters;
 import com.compomics.util.waiting.Duration;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.SQLException;
@@ -44,6 +47,7 @@ public class ReviewFigure {
     private String mgfFilePath = "C:\\Projects\\PeptideShaker\\test files\\1 mgf\\qExactive01819.mgf";
     private String fastaFilePath = "C:\\Databases\\uniprot-human-reviewed-trypsin-november-2016_concatenated_target_decoy.fasta";
     private String parametersFilePath = "C:\\Projects\\PeptideShaker\\test files\\tutorial.par";
+    private String psReportFilePath = "C:\\Github\\onyase\\Resources\\Score_test.txt";
 
     public final static String SEPARATOR = "\t";
 
@@ -51,6 +55,10 @@ public class ReviewFigure {
     private SequenceFactory sequenceFactory;
     private EnzymeFactory enzymeFactory;
     private PTMFactory ptmFactory;
+
+    private HashMap<String, String> psIds;
+    private HashMap<String, String> psScore;
+    private HashMap<String, String> psConfidence;
 
     /**
      * The main method used to start PeptideShaker.
@@ -95,6 +103,7 @@ public class ReviewFigure {
         duration.start();
 
         initializeFactories();
+        loadPsData();
 
         File mgfFile = new File(mgfFilePath);
 
@@ -110,6 +119,10 @@ public class ReviewFigure {
         searchParameters.setPrecursorAccuracyType(SearchParameters.MassAccuracyType.PPM);
         int minCharge = searchParameters.getMinChargeSearched().value;
         int maxCharge = searchParameters.getMaxChargeSearched().value;
+
+        AnnotationSettings annotationSettings = identificationParameters.getAnnotationPreferences();
+        annotationSettings.setIntensityLimit(0.5);
+        annotationSettings.clearNeutralLosses();
 
         String fileName = mgfFile.getName();
         PrecursorMap precursorMap = new PrecursorMap(spectrumFactory.getPrecursorMap(fileName), searchParameters.getPrecursorAccuracy(), searchParameters.isPrecursorAccuracyTypePpm());
@@ -127,23 +140,34 @@ public class ReviewFigure {
 
         PeptideSpectrumAnnotator peptideSpectrumAnnotator = new PeptideSpectrumAnnotator();
 
-        FastXcorr fastXcorr = new FastXcorr(PeptideFragmentationModel.uniform);
+        HyperScore hyperScore = new HyperScore(PeptideFragmentationModel.uniform);
 
         ProteinSequenceIterator proteinSequenceIterator = new ProteinSequenceIterator(searchParameters.getPtmSettings().getFixedModifications());
 
         int lastProgress = 0;
         int cpt = 0;
+        int totalProgress = sequenceFactory.getNSequences() + spectrumFactory.getNSpectra();
         SequenceFactory.ProteinIterator pi = sequenceFactory.getProteinIterator(false);
         long nMatches = 0, nSequences = 0;
 
         HashMap<String, Integer> nMatchesMap = new HashMap<String, Integer>();
+        ArrayList<String> spectrumTitlesExport = new ArrayList<String>();
         ArrayList<Double> ms1Deviations = new ArrayList<Double>();
         ArrayList<Double> scores = new ArrayList<Double>();
         ArrayList<String> categories = new ArrayList<String>();
+        HashMap<String, Double> newScores = new HashMap<String, Double>();
+        HashMap<String, Double> newEValues = new HashMap<String, Double>();
+        HashMap<String, ArrayList<Double>> hyperScores = new HashMap<String, ArrayList<Double>>(spectrumFactory.getNSpectra());
 
         while (pi.hasNext()) {
             Protein protein = pi.getNextProtein();
             String accession = protein.getAccession();
+            String category;
+            if (sequenceFactory.isDecoyAccession(accession)) {
+                category = "Decoy";
+            } else {
+                category = "Target";
+            }
             //Protein protein = sequenceFactory.getProtein("P11021");
             String sequence = protein.getSequence();
             ArrayList<Peptide> peptides = proteinSequenceIterator.getPeptides(sequence, digestionPreferences, massMin, massMax);
@@ -162,42 +186,108 @@ public class ReviewFigure {
                         Precursor precursor = precursorWithTitle.precursor;
                         String spectrumKey = Spectrum.getSpectrumKey(fileName, spectrumTitle);
                         MSnSpectrum spectrum = (MSnSpectrum) spectrumFactory.getSpectrum(fileName, spectrumTitle);
-                        AnnotationSettings annotationSettings = identificationParameters.getAnnotationPreferences();
                         SpecificAnnotationSettings specificAnnotationSettings = annotationSettings.getSpecificAnnotationPreferences(spectrumKey, peptideAssumption, identificationParameters.getSequenceMatchingPreferences(), identificationParameters.getPtmScoringPreferences().getSequenceMatchingPreferences());
-                        Double score = fastXcorr.getScore(peptide, spectrum, annotationSettings, specificAnnotationSettings, peptideSpectrumAnnotator);
-                        scores.add(score);
-                        Double ms1Deviation;
-                        if (searchParameters.isPrecursorAccuracyTypePpm()) {
-                            ms1Deviation = 1000000 * (mz - precursor.getMz()) / precursor.getMz();
-                        } else {
-                            ms1Deviation = mz - precursor.getMz();
+                        Double score = hyperScore.getScore(peptide, spectrum, annotationSettings, specificAnnotationSettings, peptideSpectrumAnnotator);
+
+                        if (score > 0) {
+                            
+                            spectrumTitlesExport.add(spectrumTitle);
+                            
+                            scores.add(score);
+
+                            ArrayList<Double> spectrumScores = hyperScores.get(spectrumTitle);
+                            if (spectrumScores == null) {
+                                spectrumScores = new ArrayList<Double>();
+                                hyperScores.put(spectrumTitle, spectrumScores);
+                            }
+                            spectrumScores.add(score);
+
+                            Double ms1Deviation;
+                            if (searchParameters.isPrecursorAccuracyTypePpm()) {
+                                ms1Deviation = 1000000 * (mz - precursor.getMz()) / precursor.getMz();
+                            } else {
+                                ms1Deviation = mz - precursor.getMz();
+                            }
+                            ms1Deviations.add(ms1Deviation);
+                            Integer matchesForSpectrum = nMatchesMap.get(spectrumTitle);
+                            if (matchesForSpectrum == null) {
+                                nMatchesMap.put(spectrumTitle, 1);
+                            } else {
+                                nMatchesMap.put(spectrumTitle, matchesForSpectrum + 1);
+                            }
+                            categories.add(category);
+
+                            String psPeptide = psIds.get(spectrumTitle);
+                            if (psPeptide != null && psPeptide.equals(peptide.getSequence())) {
+                                newScores.put(spectrumTitle, score);
+                            }
                         }
-                        ms1Deviations.add(ms1Deviation);
-                        Integer matchesForSpectrum = nMatchesMap.get(spectrumTitle);
-                        if (matchesForSpectrum == null) {
-                            nMatchesMap.put(spectrumTitle, 1);
-                        } else {
-                            nMatchesMap.put(spectrumTitle, matchesForSpectrum + 1);
-                        }
-                        String category;
-                        if (sequenceFactory.isDecoyAccession(accession)) {
-                            category = "Decoy";
-                        } else {
-                            category = "Target";
-                        }
-                        categories.add(category);
                     }
                 }
             }
 
             cpt++;
             //System.out.println(protein.getAccession() + ": " + peptides.size());
-            int progress = 100 * cpt / sequenceFactory.getNSequences();
+            int progress = 100 * cpt / totalProgress;
             if (progress > lastProgress) {
-                System.out.println("Progress: " + progress + "%");
+                System.out.println("Searching: " + progress + "%");
                 lastProgress = progress;
             }
         }
+
+        HashMap<String, HashMap<Double, Double>> eValuesMap = new HashMap<String, HashMap<Double, Double>>(hyperScores.size());
+        ArrayList<String> missingValues = new ArrayList<String>(spectrumFactory.getNSpectra());
+        for (String spectrumTitle : hyperScores.keySet()) {
+            ArrayList<Double> spectrumHyperScores = hyperScores.get(spectrumTitle);
+            boolean scored = false;
+            if (spectrumHyperScores.size() > 1) {
+                boolean debug = spectrumTitle.contains("7074.7074");
+                HashMap<Double, Double> eValues = hyperScore.getEValueHistogram(spectrumHyperScores, debug);
+                if (eValues != null) {
+                    scored = true;
+                    eValuesMap.put(spectrumTitle, eValues);
+
+                    Double newScore = newScores.get(spectrumTitle);
+                    if (newScore != null) {
+                        Double eValue = eValues.get(newScore);
+                        newEValues.put(spectrumTitle, eValue);
+                    }
+                    cpt++;
+                    int progress = 100 * cpt / totalProgress;
+                    if (progress > lastProgress) {
+                        System.out.println("Scoring: " + progress + "%");
+                        lastProgress = progress;
+                    }
+                }
+            }
+            if (!scored) {
+                missingValues.add(spectrumTitle);
+            }
+        }
+
+        Double aDefault = hyperScore.getMendianA();
+        Double bDefault = hyperScore.getMendianB();
+        for (String spectrumTitle : missingValues) {
+            ArrayList<Double> spectrumHyperScores = hyperScores.get(spectrumTitle);
+            HashMap<Double, Double> eValues = hyperScore.getInterpolation(spectrumHyperScores, aDefault, bDefault);
+            if (eValues != null) {
+                eValuesMap.put(spectrumTitle, eValues);
+
+                Double newScore = newScores.get(spectrumTitle);
+                if (newScore != null) {
+                    Double eValue = eValues.get(newScore);
+                    newEValues.put(spectrumTitle, eValue);
+                }
+                cpt++;
+                int progress = 100 * cpt / totalProgress;
+                if (progress > lastProgress) {
+                    System.out.println("Scoring: " + progress + "%");
+                    lastProgress = progress;
+                }
+            }
+        }
+
+        hyperScore.close();
 
         duration.end();
         System.out.println("Proteins: " + sequenceFactory.getNSequences() + ", Peptides: " + nSequences + ", Spectra: " + spectrumFactory.getNSpectra() + ", Matches: " + nMatches);
@@ -216,13 +306,19 @@ public class ReviewFigure {
 
         File matchesFile = new File("C:\\Github\\onyase\\R\\matches.txt");
         bw = new BufferedWriter(new FileWriter(matchesFile));
-        bw.write("MS1_deviation" + SEPARATOR + "MS2_Score" + SEPARATOR + "Category");
+        bw.write("Spectrum_Tilte" + SEPARATOR + "MS1_deviation" + SEPARATOR + "MS2_Score" + SEPARATOR + "eValue" + SEPARATOR + "Category");
         bw.newLine();
-        for (int i = 0 ; i < ms1Deviations.size() ; i++) {
+        for (int i = 0; i < spectrumTitlesExport.size(); i++) {
             Double ms1Deviation = ms1Deviations.get(i);
             Double score = scores.get(i);
             String category = categories.get(i);
-            bw.write(ms1Deviation + SEPARATOR + score + SEPARATOR + category);
+            String spectrumTitle = spectrumTitlesExport.get(i);
+            Double eValue = null;
+            HashMap<Double, Double> spectrumEValueMap = eValuesMap.get(spectrumTitle);
+            if (spectrumEValueMap != null) {
+                eValue = spectrumEValueMap.get(score);
+            }
+            bw.write(spectrumTitle + SEPARATOR + ms1Deviation + SEPARATOR + score + SEPARATOR + eValue + SEPARATOR + category);
             bw.newLine();
         }
         bw.close();
@@ -237,10 +333,47 @@ public class ReviewFigure {
             if (matchesForSpectrum == null) {
                 matchesForSpectrum = 0;
             }
-            bw.write(spectrumTitle + SEPARATOR + precursor.getMz() + SEPARATOR + precursor.getRtInMinutes()+ SEPARATOR + matchesForSpectrum);
+            bw.write(spectrumTitle + SEPARATOR + precursor.getMz() + SEPARATOR + precursor.getRtInMinutes() + SEPARATOR + matchesForSpectrum);
             bw.newLine();
         }
         bw.close();
+
+        File scoresComparisonFile = new File("C:\\Github\\onyase\\R\\scoresComparison.txt");
+        bw = new BufferedWriter(new FileWriter(scoresComparisonFile));
+        bw.write("Title" + SEPARATOR + "Sequence" + SEPARATOR + "PS score" + SEPARATOR + "PS confidence" + SEPARATOR + "New score" + SEPARATOR + "New eValue");
+        bw.newLine();
+        for (String spectrumTitle : spectrumFactory.getSpectrumTitles(fileName)) {
+            String sequence = psIds.get(spectrumTitle);
+            if (sequence != null) {
+                String score = psScore.get(spectrumTitle);
+                String confidence = psConfidence.get(spectrumTitle);
+                Double newScore = newScores.get(spectrumTitle);
+                Double eValue = newEValues.get(spectrumTitle);
+                bw.write(spectrumTitle + SEPARATOR + sequence + SEPARATOR + score + SEPARATOR + confidence + SEPARATOR + newScore + SEPARATOR + eValue);
+                bw.newLine();
+            }
+        }
+        bw.close();
+        
+        File exportFile = new File("C:\\Github\\onyase\\export\\testExample.psm");
+        bw = new BufferedWriter(new FileWriter(exportFile));
+        bw.write("Spectrum_Tilte" + SEPARATOR + "MS1_deviation" + SEPARATOR + "MS2_Score" + SEPARATOR + "eValue" + SEPARATOR + "Category");
+        bw.newLine();
+        for (int i = 0; i < spectrumTitlesExport.size(); i++) {
+            Double ms1Deviation = ms1Deviations.get(i);
+            Double score = scores.get(i);
+            String category = categories.get(i);
+            String spectrumTitle = spectrumTitlesExport.get(i);
+            Double eValue = null;
+            HashMap<Double, Double> spectrumEValueMap = eValuesMap.get(spectrumTitle);
+            if (spectrumEValueMap != null) {
+                eValue = spectrumEValueMap.get(score);
+            }
+            bw.write(spectrumTitle + SEPARATOR + ms1Deviation + SEPARATOR + score + SEPARATOR + eValue + SEPARATOR + category);
+            bw.newLine();
+        }
+        bw.close();
+        
 
         spectrumFactory.closeFiles();
         sequenceFactory.closeFile();
@@ -251,5 +384,32 @@ public class ReviewFigure {
         sequenceFactory = SequenceFactory.getInstance();
         enzymeFactory = EnzymeFactory.getInstance();
         ptmFactory = PTMFactory.getInstance();
+    }
+
+    private void loadPsData() throws IOException {
+
+        psIds = new HashMap<String, String>();
+        psScore = new HashMap<String, String>();
+        psConfidence = new HashMap<String, String>();
+
+        File psReportFile = new File(psReportFilePath);
+        BufferedReader br = new BufferedReader(new FileReader(psReportFile));
+        br.readLine();
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] split = line.split("\t");
+            String title = split[2];
+            String sequence = split[1];
+            String score = split[3];
+            String confidence = split[4];
+            String[] scoreSplit = score.split("\\(");
+            score = scoreSplit[1];
+            scoreSplit = score.split("\\)");
+            score = scoreSplit[0];
+            psIds.put(title, sequence);
+            psScore.put(title, score);
+            psConfidence.put(title, confidence);
+        }
+
     }
 }
